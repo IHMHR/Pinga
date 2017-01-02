70a3fb4a92683164e0e93d4bd200a3d51bafbda8
IHMHR <martinelli.igor@hotmail.com>
2017-01-02 15:22:13 -0200

    Ajustes
    Ajustes

--- Modelagem.sql
+++ Modelagem.sql
@@ -12,8 +12,8 @@ BEGIN
 END
 
 CREATE DATABASE pingaDB CONTAINMENT = PARTIAL
-ON (NAME = 'pingaDB', FILENAME = 'C:\Users\Martinelli\Documents\GitHub\Pinga\DB\pingaDB.mdf', SIZE = 10MB, MAXSIZE = 25MB, FILEGROWTH = 10% )
-LOG ON (NAME = 'pingaDB_LOG', FILENAME = 'C:\Users\Martinelli\Documents\GitHub\Pinga\DB\pingaDB.ldf', SIZE = 5MB, MAXSIZE = 10MB, FILEGROWTH = 20%)
+ON (NAME = 'pingaDB', FILENAME = 'C:\Users\Igor\Documents\SQL Databases\pingaDB.mdf', SIZE = 10MB, MAXSIZE = 25MB, FILEGROWTH = 10% )
+LOG ON (NAME = 'pingaDB_LOG', FILENAME = 'C:\Users\Igor\Documents\SQL Databases\pingaDB.ldf', SIZE = 5MB, MAXSIZE = 10MB, FILEGROWTH = 20%)
 COLLATE Latin1_General_CS_AS;
 GO
 
@@ -631,7 +631,7 @@ FROM pingaDB.Pinga.cliente c INNER JOIN pingaDB.Pinga.endereco e ON c.endereco =
 LEFT JOIN pingaDB.Pinga.saida s ON s.cliente = c.idcliente*/
 
 
-SELECT * FROM pingaDB.Pinga.entrada;
+--SELECT * FROM pingaDB.Pinga.entrada;
 
 
 /*18/09/2016 00:00:00
@@ -641,7 +641,7 @@ SELECT * FROM pingaDB.Pinga.entrada;
 FROM Pinga.parceiro p INNER JOIN Pinga.endereco e ON e.idendereco = p.endereco
 LEFT JOIN Pinga.saida s ON s.cliente = p.idparceiro*/
 
-INSERT INTO pingaDB.Pinga.tipo_litragem VALUES (NEWID(), 'Meiotinha'),(NEWID(), 'Litro');
+/*INSERT INTO pingaDB.Pinga.tipo_litragem VALUES (NEWID(), 'Meiotinha'),(NEWID(), 'Litro');
 
 SELECT * FROM pingaDB.Pinga.saida
 
@@ -660,7 +660,7 @@ SELECT * FROM pingaDB.Pinga.produto
 
 SELECT idproduto, p.descricao, litragem, tl.descricao, CASE WHEN p.vendendo = 1 THEN 'Vendendo' ELSE 'Fora do mercado' END AS Vendendo, p.valor_unitario
 FROM Pinga.produto p
-INNER JOIN Pinga.tipo_litragem tl ON p.tipo_litragem = tl.idtipo_litragem
+INNER JOIN Pinga.tipo_litragem tl ON p.tipo_litragem = tl.idtipo_litragem*/
 
 /*CREATE TRIGGER Pinga.trg_estoque
 ON Pinga.entrada AFTER INSERT
@@ -668,10 +668,10 @@ AS
 	INSERT INTO estoque VALUES ();
 GO*/
 
-SELECT * FROM pingaDB.Pinga.cliente;
+/*SELECT * FROM pingaDB.Pinga.cliente;
 
 
-SELECT produto, quantidade, (p.descricao + N' - ' + quantidade) AS item FROM Pinga.estoque e INNER JOIN Pinga.produto p ON p.idproduto = e.produto;
+SELECT produto, quantidade, (p.descricao + N' - ' + quantidade) AS item FROM Pinga.estoque e INNER JOIN Pinga.produto p ON p.idproduto = e.produto;*/
 
 
 
@@ -911,30 +911,6 @@ FOREIGN KEY (contrato_idcontrato) REFERENCES Pinga.contrato(idcontrato)
 --ALTER TABLE PingaDB.Pinga.visita ALTER COLUMN endereco UNIQUEIDENTIFIER NULL;
 
 
-INSERT INTO Pinga.tipo_continente (tipo_continente, ativo)
-VALUES ('Quatro continentes', 0),
-('Cinco continentes', 0),('Seis continentes', 1),
-('Seis continentes 2', 0),('Sete continentes', 0);
-
-SELECT * FROM Pinga.tipo_continente;
-
-INSERT INTO Pinga.continente (continente, tipo_continente_idtipo_continente)
-VALUES ('América', (SELECT ROWGUIDCOL FROM Pinga.tipo_continente WHERE tipo_continente = 'Quatro continentes')),('Eurafrásia', (SELECT ROWGUIDCOL FROM Pinga.tipo_continente WHERE tipo_continente = 'Quatro continentes')),('Oceania', (SELECT ROWGUIDCOL FROM Pinga.tipo_continente WHERE tipo_continente = 'Quatro continentes')),('Antártida', (SELECT ROWGUIDCOL FROM Pinga.tipo_continente WHERE tipo_continente = 'Quatro continentes')),
-('América', (SELECT ROWGUIDCOL FROM Pinga.tipo_continente WHERE tipo_continente = 'Cinco continentes')),('Ásia', (SELECT ROWGUIDCOL FROM Pinga.tipo_continente WHERE tipo_continente = 'Cinco continentes')),('Europa', (SELECT ROWGUIDCOL FROM Pinga.tipo_continente WHERE tipo_continente = 'Cinco continentes')),('África', (SELECT ROWGUIDCOL FROM Pinga.tipo_continente WHERE tipo_continente = 'Cinco continentes')),('Oceania', (SELECT ROWGUIDCOL FROM Pinga.tipo_continente WHERE tipo_continente = 'Cinco continentes')),
-('América', (SELECT ROWGUIDCOL FROM Pinga.tipo_continente WHERE tipo_continente = 'Seis continentes')),('Ásia', (SELECT ROWGUIDCOL FROM Pinga.tipo_continente WHERE tipo_continente = 'Seis continentes')),('Europa', (SELECT ROWGUIDCOL FROM Pinga.tipo_continente WHERE tipo_continente = 'Seis continentes')),('África', (SELECT ROWGUIDCOL FROM Pinga.tipo_continente WHERE tipo_continente = 'Seis continentes')),('Oceania', (SELECT ROWGUIDCOL FROM Pinga.tipo_continente WHERE tipo_continente = 'Seis continentes')),('Antártida', (SELECT ROWGUIDCOL FROM Pinga.tipo_continente WHERE tipo_continente = 'Seis continentes')),
-('América do Norte', (SELECT ROWGUIDCOL FROM Pinga.tipo_continente WHERE tipo_continente = 'Seis continentes 2')),('América do Sul', (SELECT ROWGUIDCOL FROM Pinga.tipo_continente WHERE tipo_continente = 'Seis continentes 2')),('Eurafrásia', (SELECT ROWGUIDCOL FROM Pinga.tipo_continente WHERE tipo_continente = 'Seis continentes 2')),('África', (SELECT ROWGUIDCOL FROM Pinga.tipo_continente WHERE tipo_continente = 'Seis continentes 2')),('Oceania', (SELECT ROWGUIDCOL FROM Pinga.tipo_continente WHERE tipo_continente = 'Seis continentes 2')),('Antártida', (SELECT ROWGUIDCOL FROM Pinga.tipo_continente WHERE tipo_continente = 'Seis continentes 2')),
-('América do Norte', (SELECT ROWGUIDCOL FROM Pinga.tipo_continente WHERE tipo_continente = 'Sete continentes')),('América do Sul', (SELECT ROWGUIDCOL FROM Pinga.tipo_continente WHERE tipo_continente = 'Sete continentes')),('Ásia', (SELECT ROWGUIDCOL FROM Pinga.tipo_continente WHERE tipo_continente = 'Sete continentes')),('Europa', (SELECT ROWGUIDCOL FROM Pinga.tipo_continente WHERE tipo_continente = 'Sete continentes')),('Oceania', (SELECT ROWGUIDCOL FROM Pinga.tipo_continente WHERE tipo_continente = 'Sete continentes')),('Antártida', (SELECT ROWGUIDCOL FROM Pinga.tipo_continente WHERE tipo_continente = 'Sete continentes'));
-
-SELECT * FROM Pinga.continente c, Pinga.tipo_continente tc
-WHERE tc.idtipo_continente = c.tipo_continente_idtipo_continente;
-
-INSERT INTO Pinga.pais (pais, idioma, colacao, DDI, sigla, fuso_horario, continente_idcontinente)
-VALUES ('Brasil', 'Português do Brasil', 'Latin1_General_CS_AS', '55', 'BRA', 'UTC-03:00', (SELECT c.ROWGUIDCOL FROM Pinga.continente c INNER JOIN Pinga.tipo_continente tc ON c.tipo_continente_idtipo_continente = tc.idtipo_continente WHERE tc.ativo = 1 AND c.continente = 'América'));
-
-SELECT * FROM Pinga.pais p
-INNER JOIN Pinga.continente c ON p.continente_idcontinente = c.idcontinente;
-GO
-
 IF EXISTS(SELECT 1 FROM sys.tables WHERE name = 'log_erros')
 BEGIN
     DROP TABLE adm.log_erros;
@@ -984,9 +960,9 @@ BEGIN
 END;
 GO
 
-EXEC sp_refreshsqlmodule 'adm.usp_errorLog'
-EXEC adm.usp_errorLog 'Erro', 'procedimento', 'possivelCausa', 'database';
-TRUNCATE TABLE adm.log_erros;
+--EXEC sp_refreshsqlmodule 'adm.usp_errorLog'
+--EXEC adm.usp_errorLog 'Erro', 'procedimento', 'possivelCausa', 'database';
+--TRUNCATE TABLE adm.log_erros;
 
 CREATE OR ALTER PROCEDURE Pinga.usp_InserirNovoPais
 	@pais VARCHAR(40),
@@ -1868,7 +1844,7 @@ GO
 /* USP's INSERIR CUSTO */
 
 /* USP's INSERIR VISITA */
-CREATE OR ALTER PROCEDURE Pinga.usp_InserirNovaVisita
+/*CREATE OR ALTER PROCEDURE Pinga.usp_InserirNovaVisita
 	@clienteIdcliente UNIQUEIDENTIFIER,
 	@data DATE,
 	@comecou TIME,
@@ -2036,7 +2012,7 @@ BEGIN
         THROW 51921, 'Falha ao realizar o insert do feedback.', 1;
 	END CATCH
 END;
-GO
+GO*/
 /* USP's INSERIR VISITA */
 
 CREATE OR ALTER FUNCTION Pinga.udf_IdentificarDataParaVisita (
@@ -2047,25 +2023,8 @@ RETURNS DATETIME
 WITH ENCRYPTION, SCHEMABINDING
 AS
 BEGIN
-	/*
-		IDENTIFICAR O PROXIMO DIA UTIL COM HORARIO DISPONIVEL PARA REAGENDAMENTO DA VISITA DO CLIENTE
-		OBSERVAR QUAIS OS DIAS QUE SÃO FEITAS AS VISITAS DESTE CLIENTE
-		VERIFICAR DISPONIBILIDADE NA 'AGENDA' DO PARCEIRO
-	*/
-	/*
-		SELECT 1 FROM Pinga.visita v
-		INNER JOIN Pinga.parceiro_has_visita phv
-		ON v.idvisita = phv.visita_idvisita
-		INNER JOIN Pinga.parceiro p
-		ON p.idparceiro = phv.parceiro_idparceiro
-		INNER JOIN Pinga.cliente c
-		ON c.idcliente = v.cliente_idcliente
-		WHERE v.[data] = @data AND v.comecou = @comecou
-		AND v.terminou = @terminou
-	*/
 	DECLARE @novaVisita DATETIME;
-
-	-- Regra de Negocio
+	DECLARE @proxDiaUtil DATE = (SELECT adm.udf_ProximoDiaUtil(@dataAgendada));
 
 	-- Table Variable to stored values to use
 	DECLARE @table TABLE
@@ -2101,87 +2060,6 @@ GO
 
 -- SELECT Pinga.udf_IdentificarDataParaVisita @parceiroIdparceiro, @clienteIdcliente, @data
 SELECT Pinga.udf_IdentificarDataParaVisita('4D140F96-EA43-4F8C-AC50-98FC29110F02', 'D48127E5-04EF-4F4E-A8EB-2D4F5AC21B01', GETDATE()) AS UserFunction; 
-
-
-INSERT INTO Pinga.tipo_logradouro (tipo_logradouro)
-VALUES ('Rua'), ('Avenida'), ('Alameda');
-
-INSERT INTO Pinga.tipo_complemento (tipo_complemento)
-VALUES ('Casa'), ('Apartamento'), ('Casa de esquina');
-
-INSERT INTO Pinga.estado (estado, uf, capital, pais_idpais)
-VALUES ('Minas Gerais', 'MG', 0, (SELECT ROWGUIDCOL FROM Pinga.pais WHERE pais = 'Brasil')),
-('Rio de Janeiro', 'RJ', 0, (SELECT ROWGUIDCOL FROM Pinga.pais WHERE pais = 'Brasil')),
-('São Paulo', 'SP', 0, (SELECT ROWGUIDCOL FROM Pinga.pais WHERE pais = 'Brasil'));
-
-INSERT INTO Pinga.cidade (cidade, DDD, capital, estado_idestado)
-VALUES ('Belo Horizonte', '031', 1, (SELECT ROWGUIDCOL FROM Pinga.estado WHERE uf = 'MG')),
-('Contagem', '031', 0, (SELECT ROWGUIDCOL FROM Pinga.estado WHERE uf = 'MG')),
-('Abaeté', '035', 0, (SELECT ROWGUIDCOL FROM Pinga.estado WHERE uf = 'MG'));
-
-INSERT INTO Pinga.bairro (bairro, regiao, cidade_idcidade)
-VALUES ('Alípio de Melo', 'Noroeste', (SELECT ROWGUIDCOL FROM Pinga.cidade WHERE cidade = 'Belo Horizonte')),
-('Barroca', 'Oeste', (SELECT ROWGUIDCOL FROM Pinga.cidade WHERE cidade = 'Belo Horizonte'));
-
-INSERT INTO Pinga.endereco (tipo_logradouro_idtipo_logradouro, logradouro, numero, tipo_complemento_idtipo_complemento, complemento, CEP, ponto_referencia, bairro_idbairro, created)
-VALUES ((SELECT ROWGUIDCOL FROM Pinga.tipo_logradouro WHERE tipo_logradouro = 'Rua'), 'dos Securitários', 115, (SELECT ROWGUIDCOL FROM Pinga.tipo_complemento WHERE tipo_complemento = 'Casa'), 'com grade branca', '30840760', 'Próxima a padaria Gamela', (SELECT ROWGUIDCOL FROM Pinga.bairro WHERE bairro = 'Alípio de Melo'), GETDATE());
-
-
-INSERT INTO Pinga.tipo_telefone (descricao)
-VALUES ('Telefone Residencial'),('Celular'),('Telefone Comercial'),('Telefone Extra');
-
-INSERT INTO Pinga.operadora (nome, razao_social, status)
-VALUES ('TIM', 'Telecom Italia Mobile', 1);
-
-INSERT INTO Pinga.telefone (telefone, cidade_ddd, tipo_telefone_idtipo_telefone, operadora_idoperadora, created)
-VALUES ('988521996', (SELECT ROWGUIDCOL FROM Pinga.cidade WHERE DDD = '031' AND cidade = 'Belo Horizonte'), (SELECT ROWGUIDCOL FROM Pinga.tipo_telefone WHERE descricao = 'Celular'), (SELECT ROWGUIDCOL FROM Pinga.operadora WHERE nome = 'TIM'), GETDATE());
-
-INSERT INTO Pinga.parceiro (nome, endereco_idendereco, status, telefone_idtelefone, created)
-VALUES ('Parceiro A', (SELECT ROWGUIDCOL FROM Pinga.endereco WHERE CEP = '30840760' AND logradouro = 'dos Securitários'), 1, (SELECT ROWGUIDCOL FROM Pinga.telefone WHERE telefone = '988521996'), GETDATE()),
-('Parceiro B', (SELECT ROWGUIDCOL FROM Pinga.endereco WHERE CEP = '30840760' AND logradouro = 'dos Securitários'), 1, (SELECT ROWGUIDCOL FROM Pinga.telefone WHERE telefone = '988521996'), GETDATE()),
-('Parceiro C', (SELECT ROWGUIDCOL FROM Pinga.endereco WHERE CEP = '30840760' AND logradouro = 'dos Securitários'), 1, (SELECT ROWGUIDCOL FROM Pinga.telefone WHERE telefone = '988521996'), GETDATE());
-
-
-INSERT INTO Pinga.email_localidade (email_localidade, status)
-VALUES ('br', 1),('it', 1),('co', 1),('jp', 1),('com.br', 1), ('com', 1);
-
-INSERT INTO Pinga.email_dominio (email_dominio, status)
-VALUES ('hotmail', 1),('outlook', 1),('yahoo', 1);
-
-INSERT INTO Pinga.email (email, email_dominio_idemail_dominio, email_localidade_idemail_localidade)
-VALUES ('martinelli.igor', (SELECT ROWGUIDCOL FROM Pinga.email_dominio WHERE email_dominio = 'hotmail'), (SELECT ROWGUIDCOL FROM Pinga.email_localidade WHERE email_localidade = 'com'));
-
-INSERT INTO Pinga.cliente (cpf_cnpj,nome_razao_social, apelido_nome_fantasia, inscricao_municipal, identidade_inscricao_estadual, data_nascimento_fundacao, sexo, email_idemail, endereco_idendereco, telefone_idtelefone, created)
-VALUES ('13089902605', 'Igor Henrique Martinelli de Heredia Ramos', 'IHMHR', '13089902605', 'MG17771898', '1996-09-22', 'M', (SELECT e.ROWGUIDCOL FROM Pinga.email e INNER JOIN Pinga.email_dominio em ON em.idemail_dominio = e.email_dominio_idemail_dominio INNER JOIN Pinga.email_localidade el ON el.idemail_localidade = e.email_localidade_idemail_localidade WHERE e.email = 'martinelli.igor' AND em.email_dominio = 'hotmail' AND el.email_localidade = 'com'), (SELECT ROWGUIDCOL FROM Pinga.endereco WHERE CEP = '30840760' AND logradouro = 'dos Securitários'), (SELECT ROWGUIDCOL FROM Pinga.telefone WHERE telefone = '988521996'), GETDATE());
-
-
-INSERT INTO Pinga.visita (cliente_idcliente, data, comecou, terminou)
-VALUES ('D48127E5-04EF-4F4E-A8EB-2D4F5AC21B01', GETDATE(), '15:00', '15:15'),
-('D48127E5-04EF-4F4E-A8EB-2D4F5AC21B01', GETDATE(), '15:00', '15:15'),
-('D48127E5-04EF-4F4E-A8EB-2D4F5AC21B01', GETDATE(), '15:30', '15:45'),
-('D48127E5-04EF-4F4E-A8EB-2D4F5AC21B01', GETDATE(), '16:00', '16:15');
-
-INSERT INTO Pinga.feedback_visita (visita_idvisita, comentario, nota, venda_realizada, visita_reagendada, created)
-VALUES ('9DE53D8F-9B54-4386-85E3-328A045872A7', 'comentario', 10, 1, 1, GETDATE());
-
-INSERT INTO Pinga.parceiro_has_visita (parceiro_idparceiro, visita_idvisita)
-VALUES ('4D140F96-EA43-4F8C-AC50-98FC29110F02', '9DE53D8F-9B54-4386-85E3-328A045872A7');
-
-INSERT INTO Pinga.parceiro_has_visita (parceiro_idparceiro, visita_idvisita)
-VALUES ('4D140F96-EA43-4F8C-AC50-98FC29110F02', '45076C04-E3D5-4F5F-A4FA-6510186AA82D')
-,('4D140F96-EA43-4F8C-AC50-98FC29110F02', 'BB315B67-BBBE-40C8-8AD5-DC03FA9B443E')
-,('4D140F96-EA43-4F8C-AC50-98FC29110F02', '7777F6EA-8B26-48F9-8885-B9A3590320E3')
-,('4D140F96-EA43-4F8C-AC50-98FC29110F02', '12D61FE3-8949-400F-AAD4-6610378742D4')
-,('4D140F96-EA43-4F8C-AC50-98FC29110F02', '77FA9E7B-A9DE-44D3-907E-B656C74764ED')
-,('4D140F96-EA43-4F8C-AC50-98FC29110F02', '57C18D8D-79D7-4DBD-857A-EDA171EA7438')
-,('4D140F96-EA43-4F8C-AC50-98FC29110F02', 'C912999C-520B-426B-811F-151AA37A69D0');
-
-
-INSERT INTO Pinga.visita (cliente_idcliente, data, comecou, terminou)
-VALUES ('D48127E5-04EF-4F4E-A8EB-2D4F5AC21B01', DATEADD(d, -2, GETDATE()), '15:00', '15:15'),
-('D48127E5-04EF-4F4E-A8EB-2D4F5AC21B01', DATEADD(d, -3, GETDATE()), '15:00', '15:15'),
-('D48127E5-04EF-4F4E-A8EB-2D4F5AC21B01', DATEADD(d, -4, GETDATE()), '15:30', '15:45'),
-('D48127E5-04EF-4F4E-A8EB-2D4F5AC21B01', DATEADD(d, -7, GETDATE()), '16:00', '16:15');
 
 
 IF EXISTS(SELECT 1 FROM sys.tables WHERE name = 'horario')
