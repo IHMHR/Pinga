54323e459396a0daaa96abaed4d64822a2171706
IHMHR <martinelli.igor@hotmail.com>
2017-01-07 01:43:22 -0200

    PHP Solution
    PHP Access to MSSQL

---
+++ Pinga PHP/DAL.php
@@ -0,0 +1,52 @@
+<?php
+/**
+* Class that make a connection with SQL Server
+* @author IHMHR
+*/
+require_once("constantes.php");
+
+class DAL
+{
+	private $pdo = null;
+
+	function __construct()
+	{
+	}
+
+	function abre_banco()
+	{
+		if($this->pdo)
+		{
+			return $this->pdo;
+		}
+
+		try
+		{
+			//return $this->pdo = new PDO("mssql:host=" . SERVER . ";dbname=" . DATABASE . "",	USER_ID, PASSWORD);
+			//return $this->pdo = new PDO("sqlsrv:host=" . SERVER . " dbname=" . DATABASE . "", USER_ID, PASSWORD);
+			//return $this->pdo = new PDO("sqlsrv:host=" . SERVER . ";dbname=" . DATABASE . "", USER_ID, PASSWORD);
+			return $this->pdo = new PDO("sqlsrv:Server=" . SERVER . ";Database=" . DATABASE, USER_ID, PASSWORD); 
+			//$conn = new PDO("sqlsrv:Server=localhost;Database=testdb", "UserName", "Password"); 
+		}
+		catch (PDOException $e)
+		{
+    		echo "Erro de Conexão \n" . $e . "\n";
+    		return FALSE;
+  		}
+		/*if($conn)
+		{
+			return $conn;
+		}
+		else
+		{
+			return $conn = sqlsrv_connect($svr, $cntInfo);
+		}*/
+	}
+
+	function fecha_banco($connection)
+	{
+		//sqlsrv_close($connection);
+	}
+}
+
+?>	
---
+++ Pinga PHP/constantes.php
@@ -0,0 +1,7 @@
+<?php
+	define("SERVER", ".\SQLEXPRESS");
+	//define("CONNECTION_INFO", array('Database' => 'pingaDB', 'UID' => 'Tst', 'PWD' => '220996'));
+	define("DATABASE", "pingaDB");
+	define("USER_ID", "sa");
+	define("PASSWORD", "220996");
+?>
