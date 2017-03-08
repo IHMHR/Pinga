029f7da8b526033cc2582e16e749dc1a76416b6b
IHMHR <martinelli.igor@hotmail.com>
2017-03-08 20:28:36 -0300

    Ajuste

---
+++ Pinga/BLL/Classes/ClsEntradaHasItem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Classes
+{
+    class ClsEntradaHasItem : IGeneric<ClsEntradaHasItem>
+    {
+        public void Alterar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Apagar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClsEntradaHasItem BuscaPeloId(Guid rowGuidCol)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Inserir()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ValidarClasse(CRUD crud)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ClsEntradaHasItem> Visualizar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Pinga/BLL/Classes/ClsSaidaHasItem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Classes
+{
+    class ClsSaidaHasItem : IGeneric<ClsSaidaHasItem>
+    {
+        public Guid idsaida_has_ { get; set; }
+
+        public void Alterar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Apagar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClsSaidaHasItem BuscaPeloId(Guid rowGuidCol)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Inserir()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ValidarClasse(CRUD crud)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ClsSaidaHasItem> Visualizar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
