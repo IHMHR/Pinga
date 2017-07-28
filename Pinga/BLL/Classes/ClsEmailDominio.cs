315566e545d85351b5482334ea156c10f1e71510
Igor Martinelli Ramos <martinelli.igor@hotmail.com>
2017-07-28 12:06:24 -0300

    Removendo metodos das classes

--- Pinga/BLL/Classes/ClsEmailDominio.cs
+++ Pinga/BLL/Classes/ClsEmailDominio.cs
@@ -8,55 +8,5 @@ namespace BLL.Classes
         public Guid idemailDominio { get; set; }
         public string emailDominio { get; set; }
         public bool status { get; set; }
-
-        public void Inserir()
-        { }
-
-        public void Alterar()
-        { }
-
-        public void Apagar()
-        { }
-
-        public List<ClsEmailDominio> Visualizar()
-        {
-            return null;
-        }
-
-        public void ValidarClasse(CRUD crud)
-        {
-            if (crud == CRUD.insert)
-            {
-                if (string.IsNullOrEmpty(emailDominio.Trim()))
-                {
-                    throw new ArgumentNullException("Por favor informe o Dominio do email");
-                }
-                else if (status != true && status != false)
-                {
-                    throw new ArgumentNullException("Por favor informe o status do Dominio do email");
-                }
-            }
-            else if (crud == CRUD.update)
-            {
-                ValidarClasse(CRUD.insert);
-                ValidarClasse(CRUD.delete);
-            }
-            else if (crud == CRUD.delete)
-            {
-                if (idemailDominio.ToString() == "00000000-0000-0000-0000-000000000000")
-                {
-                    throw new ArgumentNullException("Por favor informe o ID do Dominio do email");
-                }
-            }
-            else
-            {
-                throw new ArgumentException("Falha interna do Programar ao informar qual operação deve ser validada.");
-            }
-        }
-
-        public ClsEmailDominio BuscaPeloId(Guid rowGuidCol)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
--- Pinga/BLL/Classes/ClsEmailLocalidade.cs
+++ Pinga/BLL/Classes/ClsEmailLocalidade.cs
@@ -8,55 +8,5 @@ namespace BLL.Classes
         public Guid idemailLocalidade { get; set; }
         public string emailLocalidade { get; set; }
         public bool status { get; set; }
-
-        public void Inserir()
-        { }
-
-        public void Alterar()
-        { }
-
-        public void Apagar()
-        { }
-
-        public List<ClsEmailLocalidade> Visualizar()
-        {
-            return null;
-        }
-
-        public void ValidarClasse(CRUD crud)
-        {
-            if (crud == CRUD.insert)
-            {
-                if (string.IsNullOrEmpty(emailLocalidade.Trim()))
-                {
-                    throw new ArgumentNullException("Por favor informe a Localidade do email");
-                }
-                else if (status != true && status != false)
-                {
-                    throw new ArgumentNullException("Por favor informe o status da Localidade do email");
-                }
-            }
-            else if (crud == CRUD.update)
-            {
-                ValidarClasse(CRUD.insert);
-                ValidarClasse(CRUD.delete);
-            }
-            else if (crud == CRUD.delete)
-            {
-                if (idemailLocalidade.ToString() == "00000000-0000-0000-0000-000000000000")
-                {
-                    throw new ArgumentNullException("Por favor informe o ID da Localidade do email");
-                }
-            }
-            else
-            {
-                throw new ArgumentException("Falha interna do Programar ao informar qual operação deve ser validada.");
-            }
-        }
-
-        public ClsEmailLocalidade BuscaPeloId(Guid rowGuidCol)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
